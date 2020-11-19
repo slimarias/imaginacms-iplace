318b647c2254faa5c606250e6e4fca4eebf8be2f
Slim Arias Sanchez <slimarias1987@gmail.com>
2020-11-19 18:16:32 -0500

    +ADD: Public routes with localization

--- Http/frontendRoutes.php
+++ Http/frontendRoutes.php
@@ -8,14 +8,32 @@ $router->group(['prefix' => 'lugares'], function (Router $router) {
 
     $router->get('/', [
         'as' => 'iplaces.place.index',
-        'uses' => 'PublicController@index',
+        'uses' => 'OldPublicController@index',
     ]);
     $router->get('/{slugcategory}', [
         'as' => 'iplaces.place.category',
-        'uses' => 'PublicController@category',
+        'uses' => 'OldPublicController@category',
     ]);
     $router->get('{slugcategory}/{slugplace}', [
         'as' => 'iplaces.place.show',
+        'uses' => 'OldPublicController@show',
+    ]);
+
+});
+
+$router->group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['localize']], function (Router $router) {
+    $locale = LaravelLocalization::setLocale() ?: App::getLocale();
+
+    $router->get(trans('iplaces::routes.places.index.index'), [
+        'as' => $locale.'.iplaces.place.index',
+        'uses' => 'PublicController@index',
+    ]);
+    $router->get(trans('iplaces::routes.places.index.category'), [
+        'as' => $locale .'.iplaces.place.category',
+        'uses' => 'PublicController@index',
+    ]);
+    $router->get(trans('iplaces::routes.places.index.place'), [
+        'as' => $locale.'.iplaces.place.show',
         'uses' => 'PublicController@show',
     ]);
 
