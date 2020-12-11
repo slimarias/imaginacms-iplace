07e94ab447c2a4ff07227f3e5adef22f03d5dec5
Slim Arias Sanchez <slimarias1987@gmail.com>
2020-12-11 10:56:14 -0500

    +UPDATE

--- Repositories/Eloquent/EloquentCategoryRepository.php
+++ Repositories/Eloquent/EloquentCategoryRepository.php
@@ -12,143 +12,143 @@ use Modules\Iplaces\Events\CategoryWasCreated;
 class EloquentCategoryRepository extends EloquentBaseRepository implements CategoryRepository
 {
 
-  public function getItemsBy($params = false)
-  {
-    /*== initialize query ==*/
-    $query = $this->model->query();
+    public function getItemsBy($params = false)
+    {
+        /*== initialize query ==*/
+        $query = $this->model->query();
 
-    /*== RELATIONSHIPS ==*/
-    if (in_array('*', $params->include)) {//If Request all relationships
-      $query->with([]);
-    } else {//Especific relationships
-      $includeDefault = [];//Default relationships
-      if (isset($params->include))//merge relations with default relationships
-        $includeDefault = array_merge($includeDefault, $params->include);
-      $query->with($includeDefault);//Add Relationships to query
+        /*== RELATIONSHIPS ==*/
+        if (in_array('*', $params->include)) {//If Request all relationships
+            $query->with([]);
+        } else {//Especific relationships
+            $includeDefault = [];//Default relationships
+            if (isset($params->include))//merge relations with default relationships
+                $includeDefault = array_merge($includeDefault, $params->include);
+            $query->with($includeDefault);//Add Relationships to query
+        }
+
+        /*== FILTERS ==*/
+        if (isset($params->filter)) {
+            $filter = $params->filter;//Short filter
+
+            //Filter by date
+            if (isset($filter->date)) {
+                $date = $filter->date;//Short filter date
+                $date->field = $date->field ?? 'created_at';
+                if (isset($date->from))//From a date
+                    $query->whereDate($date->field, '>=', $date->from);
+                if (isset($date->to))//to a date
+                    $query->whereDate($date->field, '<=', $date->to);
+            }
+
+            //Order by
+            if (isset($filter->order)) {
+                $orderByField = $filter->order->field ?? 'created_at';//Default field
+                $orderWay = $filter->order->way ?? 'desc';//Default way
+                $query->orderBy($orderByField, $orderWay);//Add order to query
+            }
+
+            //add filter by search
+            if (isset($filter->search)) {
+                //find search in columns
+                $query->where('id', 'like', '%' . $filter->search . '%')
+                    ->orWhere('updated_at', 'like', '%' . $filter->search . '%')
+                    ->orWhere('created_at', 'like', '%' . $filter->search . '%');
+            }
+        }
+
+        /*== FIELDS ==*/
+        if (isset($params->fields) && count($params->fields))
+            $query->select($params->fields);
+
+        /*== REQUEST ==*/
+        if (isset($params->page) && $params->page) {
+            return $query->paginate($params->take);
+        } else {
+            $params->take ? $query->take($params->take) : false;//Take
+            return $query->get();
+        }
     }
 
-    /*== FILTERS ==*/
-    if (isset($params->filter)) {
-      $filter = $params->filter;//Short filter
 
-      //Filter by date
-      if (isset($filter->date)) {
-        $date = $filter->date;//Short filter date
-        $date->field = $date->field ?? 'created_at';
-        if (isset($date->from))//From a date
-          $query->whereDate($date->field, '>=', $date->from);
-        if (isset($date->to))//to a date
-          $query->whereDate($date->field, '<=', $date->to);
-      }
+    public function getItem($criteria, $params = false)
+    {
+        //Initialize query
+        $query = $this->model->query();
 
-      //Order by
-      if (isset($filter->order)) {
-        $orderByField = $filter->order->field ?? 'created_at';//Default field
-        $orderWay = $filter->order->way ?? 'desc';//Default way
-        $query->orderBy($orderByField, $orderWay);//Add order to query
-      }
+        /*== RELATIONSHIPS ==*/
+        if (in_array('*', $params->include)) {//If Request all relationships
+            $query->with([]);
+        } else {//Especific relationships
+            $includeDefault = [];//Default relationships
+            if (isset($params->include))//merge relations with default relationships
+                $includeDefault = array_merge($includeDefault, $params->include);
+            $query->with($includeDefault);//Add Relationships to query
+        }
 
-      //add filter by search
-      if (isset($filter->search)) {
-        //find search in columns
-        $query->where('id', 'like', '%' . $filter->search . '%')
-          ->orWhere('updated_at', 'like', '%' . $filter->search . '%')
-          ->orWhere('created_at', 'like', '%' . $filter->search . '%');
-      }
+        /*== FILTER ==*/
+        if (isset($params->filter)) {
+            $filter = $params->filter;
+
+            if (isset($filter->field))//Filter by specific field
+                $field = $filter->field;
+        }
+
+        /*== FIELDS ==*/
+        if (isset($params->fields) && count($params->fields))
+            $query->select($params->fields);
+
+        /*== REQUEST ==*/
+        return $query->where($field ?? 'id', $criteria)->first();
     }
 
-    /*== FIELDS ==*/
-    if (isset($params->fields) && count($params->fields))
-      $query->select($params->fields);
-
-    /*== REQUEST ==*/
-    if (isset($params->page) && $params->page) {
-      return $query->paginate($params->take);
-    } else {
-      $params->take ? $query->take($params->take) : false;//Take
-      return $query->get();
-    }
-  }
-
-
-  public function getItem($criteria, $params = false)
-  {
-    //Initialize query
-    $query = $this->model->query();
-
-    /*== RELATIONSHIPS ==*/
-    if (in_array('*', $params->include)) {//If Request all relationships
-      $query->with([]);
-    } else {//Especific relationships
-      $includeDefault = [];//Default relationships
-      if (isset($params->include))//merge relations with default relationships
-        $includeDefault = array_merge($includeDefault, $params->include);
-      $query->with($includeDefault);//Add Relationships to query
+    public function create($data)
+    {
+        $category= $this->model->create($data);
+        event(new CategoryWasCreated($category, $data));
+        event(new CreateMedia($category,$data));
+        return $this->find($category->id);
     }
 
-    /*== FILTER ==*/
-    if (isset($params->filter)) {
-      $filter = $params->filter;
 
-      if (isset($filter->field))//Filter by specific field
-        $field = $filter->field;
+    public function updateBy($criteria, $data, $params = false)
+    {
+        /*== initialize query ==*/
+        $query = $this->model->query();
+
+        /*== FILTER ==*/
+        if (isset($params->filter)) {
+            $filter = $params->filter;
+
+            //Update by field
+            if (isset($filter->field))
+                $field = $filter->field;
+        }
+
+        /*== REQUEST ==*/
+        $model = $query->where($field ?? 'id', $criteria)->first();
+
+        $model ? $model->update((array)$data) : false;
+        event(new UpdateMedia($model,$data));
     }
 
-    /*== FIELDS ==*/
-    if (isset($params->fields) && count($params->fields))
-      $query->select($params->fields);
+    public function deleteBy($criteria, $params = false)
+    {
+        /*== initialize query ==*/
+        $query = $this->model->query();
 
-    /*== REQUEST ==*/
-    return $query->where($field ?? 'id', $criteria)->first();
-  }
+        /*== FILTER ==*/
+        if (isset($params->filter)) {
+            $filter = $params->filter;
 
-  public function create($data)
-  {
-      $category= $this->model->create($data);
-      event(new CategoryWasCreated($category, $data));
-    event(new CreateMedia($category,$data));
-      return $this->find($category->id);
-  }
+            if (isset($filter->field))//Where field
+                $field = $filter->field;
+        }
 
-
-  public function updateBy($criteria, $data, $params = false)
-  {
-    /*== initialize query ==*/
-    $query = $this->model->query();
-
-    /*== FILTER ==*/
-    if (isset($params->filter)) {
-      $filter = $params->filter;
-
-      //Update by field
-      if (isset($filter->field))
-        $field = $filter->field;
+        /*== REQUEST ==*/
+        $model = $query->where($field ?? 'id', $criteria)->first();
+        $model ? $model->delete() : false;
+        event(new DeleteMedia($model->id, get_class($model)));
     }
-
-    /*== REQUEST ==*/
-    $model = $query->where($field ?? 'id', $criteria)->first();
-
-    $model ? $model->update((array)$data) : false;
-    event(new UpdateMedia($model,$data));
-  }
-
-  public function deleteBy($criteria, $params = false)
-  {
-    /*== initialize query ==*/
-    $query = $this->model->query();
-
-    /*== FILTER ==*/
-    if (isset($params->filter)) {
-      $filter = $params->filter;
-
-      if (isset($filter->field))//Where field
-        $field = $filter->field;
-    }
-
-    /*== REQUEST ==*/
-    $model = $query->where($field ?? 'id', $criteria)->first();
-    $model ? $model->delete() : false;
-    event(new DeleteMedia($model->id, get_class($model)));
-  }
 
 }
